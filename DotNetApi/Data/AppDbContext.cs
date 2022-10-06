f4d3034bca5a0c6e6c9e9060381053104f7fad97
Scott Vaughan <scott.vaughan@ensono.com>
2022-10-06 11:53:31 +0100

    Create basic API

---
+++ DotNetApi/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DotNetApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetApi.Data;
+
+public class AppDbContext : DbContext
+{
+	public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+	{
+
+	}
+
+	public DbSet<ToDo> ToDos => Set<ToDo>();
+}
--- DotNetApi/Program.cs
+++ DotNetApi/Program.cs
@@ -1,43 +1,35 @@
+using DotNetApi.Data;
+using DotNetApi.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlite(builder.Configuration.GetConnectionString("SqlLiteDb")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseHttpsRedirection(); // Redirects to https endpoint.
 
-var summaries = new[]
+app.MapGet("api/todos", async (AppDbContext context) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return Results.Ok(await context.ToDos.ToListAsync());
 })
-.WithName("GetWeatherForecast");
+.WithName("Get ToDos");
+
+app.MapPost("api/todo", async (AppDbContext context, ToDo toDo) =>
+{
+    await context.ToDos.AddAsync(toDo);
+    await context.SaveChangesAsync();
+    return Results.Created($"api/todos/{toDo.Id}", toDo);
+})
+.WithName("Create ToDo");
 
 app.Run();
-
-record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
